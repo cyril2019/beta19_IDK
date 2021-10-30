6d408d45c458a4ee107526c59065d96b9d631dc3
dangerdude237 <dhanush2019@iiitkottayam.ac.in>
2021-10-30 09:46:05 +0300

    added small form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import "./App.css";
 
-function clickMe() {}
 
-function App() {
-  return (
+
+
+function App() 
+{
+  const [myCar, setMyCar] = useState("candidate1");
+
+  const handleChange = (event) => {
+    setMyCar(event.target.value)
+}
+
+  return 
+  (
     <>
-      <div>
-        <button onClick={clickMe}>Button1</button>
-      </div>
-
-      <div>
-        <button onClick={clickMe}>Button2</button>
-      </div>
-      <div>
-        <button onClick={clickMe}>Button3</button>
-      </div>
-      <div>
-        <button onClick={clickMe}>Button4</button>
-      </div>
+    <form>
+      <select value={selected_candidate} onChange={handleChange}>
+        <option value="candidate1">candidate1</option>
+        <option value="candidate2" selected>candidate2</option>
+        <option value="candidate3">candidate3</option>
+      </select>
+    </form>
     </>
   );
 }
